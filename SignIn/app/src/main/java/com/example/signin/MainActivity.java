0c8287bcbc0c1554cb2788e1d937be43ea3ddb78
Gopika <gopugrasp@gmail.com>
2021-12-27 22:15:05 +0530

    signin and navigation

---
+++ SignIn/app/src/main/java/com/example/signin/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentProviderClient;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText Name,Pass;
+    private Button Login;
+
+    private int counter=5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Name =(EditText) findViewById(R.id.etnusername);
+        Pass =(EditText) findViewById(R.id.etnpass);
+
+        Login =(Button) findViewById(R.id.etnlogin);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Pass.getText().toString());
+            }
+        });
+
+    }
+    private void validate(String username ,String password){
+        if((username.equals("Gopika"))&&(password.equals("Gopika@124")))
+        {
+            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
+
+            if(counter==0){
+                Login.setEnabled(false);
+            }
+
+        }
+    }
+}
